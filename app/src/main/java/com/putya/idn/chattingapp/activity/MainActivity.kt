00441fad625d37729bf2c1619cb8eeea7f46bce6
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2020-07-20 17:21:27 +0700

    okay last sumpah fix

--- app/src/main/java/com/putya/idn/chattingapp/activity/MainActivity.kt
+++ app/src/main/java/com/putya/idn/chattingapp/activity/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 import com.putya.idn.chattingapp.R
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -23,10 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(tool_bar)
 
         val toolBar: Toolbar = findViewById(R.id.tool_bar)
-        setActionBar(toolBar)
+        setSupportActionBar(toolBar)
         supportActionBar!!.title = ""
 
         val tabLayout: TabLayout = findViewById(R.id.tab_layout)
--- app/src/main/java/com/putya/idn/chattingapp/activity/SignUpActivity.kt
+++ app/src/main/java/com/putya/idn/chattingapp/activity/SignUpActivity.kt
@@ -4,11 +4,96 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.putya.idn.chattingapp.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
+
+//class SignupActivity : AppCompatActivity() {
+//
+//    private lateinit var mAuth: FirebaseAuth
+//    private lateinit var refUsers: DatabaseReference
+//    private var firebaseUserID: String = ""
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_sign_up)
+//
+//        val toolBar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_signup)
+//        setSupportActionBar(toolBar)
+//
+//        supportActionBar!!.title = getString(R.string.text_signup)
+//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+//        toolBar.setNavigationOnClickListener {
+//            val intent = Intent(this, WelcomeActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+//        mAuth = FirebaseAuth.getInstance()
+//        btn_signup.setOnClickListener {
+//            signupUser()
+//        }
+//    }
+//
+//    private fun signupUser() {
+//        val username: String = et_user_name_signup.text.toString()
+//        val email: String = et_email.text.toString()
+//        val password: String = et_password.text.toString()
+//
+//        if (username == "") {
+//            Toast.makeText(this, getString(R.string.text_message_username), Toast.LENGTH_LONG)
+//                .show()
+//
+//        } else if (email == "") {
+//            Toast.makeText(this, getString(R.string.text_message_email), Toast.LENGTH_LONG).show()
+//
+//        } else if (password == "") {
+//            Toast.makeText(this, getString(R.string.text_message_password), Toast.LENGTH_LONG)
+//                .show()
+//
+//        } else {
+//
+//            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    firebaseUserID = mAuth.currentUser!!.uid
+//                    refUsers = FirebaseDatabase.getInstance()
+//                        .reference.child(getString(R.string.text_user)).child(firebaseUserID)
+//
+//                    val userHashMap = HashMap<String, Any>()
+//                    userHashMap["uid"] = firebaseUserID
+//                    userHashMap["username"] = username
+//                    userHashMap["profile"] =
+//                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-22b4a.appspot.com/o/profile.png?alt=media&token=e70fbb32-e7b8-40fe-83e8-a199404b1125"
+//                    userHashMap["cover"] =
+//                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-22b4a.appspot.com/o/cover.jpeg?alt=media&token=e98859f9-90cd-4932-9aea-7b18b39f10f4"
+//                    userHashMap["status"] = "offline"
+//                    userHashMap["search"] = username.toLowerCase()
+//                    userHashMap["facebook"] = "https://m.facebook.com"
+//                    userHashMap["instagram"] = "https://m.instagram.com"
+//                    userHashMap["website"] = "https://www.google.com"
+//
+//                    refUsers.updateChildren(userHashMap).addOnCompleteListener { task ->
+//                        if (task.isSuccessful) {
+//                            val intent = Intent(this, MainActivity::class.java)
+//                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+//                            startActivity(intent)
+//                            finish()
+//                        }
+//                    }
+//
+//                } else {
+//                    Toast.makeText(
+//                        this, getString(R.string.text_error_message) + task.exception!!
+//                            .message.toString(), Toast.LENGTH_LONG
+//                    ).show()
+//
+//                }
+//            }
+//        }
+//    }
+//}
 
 class SignupActivity : AppCompatActivity() {
 
@@ -20,7 +105,7 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val toolBar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_signup)
+        val toolBar: Toolbar = findViewById(R.id.toolbar_signup)
         setSupportActionBar(toolBar)
 
         supportActionBar!!.title = getString(R.string.text_signup)
@@ -64,9 +149,9 @@ class SignupActivity : AppCompatActivity() {
                     userHashMap["uid"] = firebaseUserID
                     userHashMap["username"] = username
                     userHashMap["profile"] =
-                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-22b4a.appspot.com/o/profile.png?alt=media&token=e70fbb32-e7b8-40fe-83e8-a199404b1125"
+                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-9f61f.appspot.com/o/profile.png?alt=media&token=20b2a932-5e54-4110-8dd5-f32e0e3c6784"
                     userHashMap["cover"] =
-                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-22b4a.appspot.com/o/cover.jpeg?alt=media&token=e98859f9-90cd-4932-9aea-7b18b39f10f4"
+                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-9f61f.appspot.com/o/cover.jpeg?alt=media&token=7f8044c8-41d7-499f-b6c2-c7522cc4a872"
                     userHashMap["status"] = "offline"
                     userHashMap["search"] = username.toLowerCase()
                     userHashMap["facebook"] = "https://m.facebook.com"
@@ -81,7 +166,6 @@ class SignupActivity : AppCompatActivity() {
                             finish()
                         }
                     }
-
                 } else {
                     Toast.makeText(
                         this, getString(R.string.text_error_message) + task.exception!!
