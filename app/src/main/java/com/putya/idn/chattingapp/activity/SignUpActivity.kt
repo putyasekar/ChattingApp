5b9ceac1a1be566bf6a66eac63b90d9b2ac9a8b9
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2020-07-20 16:18:45 +0700

    peek a boo! here's the update son

--- app/src/main/java/com/putya/idn/chattingapp/activity/SignUpActivity.kt
+++ app/src/main/java/com/putya/idn/chattingapp/activity/SignUpActivity.kt
@@ -1,12 +1,95 @@
 package com.putya.idn.chattingapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.putya.idn.chattingapp.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var refUsers: DatabaseReference
+    private var firebaseUserID: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val toolBar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_signup)
+        setSupportActionBar(toolBar)
+
+        supportActionBar!!.title = getString(R.string.text_signup)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolBar.setNavigationOnClickListener {
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        mAuth = FirebaseAuth.getInstance()
+        btn_signup.setOnClickListener {
+            signupUser()
+        }
+    }
+
+    private fun signupUser() {
+        val username: String = et_user_name_signup.text.toString()
+        val email: String = et_email.text.toString()
+        val password: String = et_password.text.toString()
+
+        if (username == "") {
+            Toast.makeText(this, getString(R.string.text_message_username), Toast.LENGTH_LONG)
+                .show()
+
+        } else if (email == "") {
+            Toast.makeText(this, getString(R.string.text_message_email), Toast.LENGTH_LONG).show()
+
+        } else if (password == "") {
+            Toast.makeText(this, getString(R.string.text_message_password), Toast.LENGTH_LONG)
+                .show()
+
+        } else {
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseUserID = mAuth.currentUser!!.uid
+                    refUsers = FirebaseDatabase.getInstance()
+                        .reference.child(getString(R.string.text_user)).child(firebaseUserID)
+
+                    val userHashMap = HashMap<String, Any>()
+                    userHashMap["uid"] = firebaseUserID
+                    userHashMap["username"] = username
+                    userHashMap["profile"] =
+                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-22b4a.appspot.com/o/profile.png?alt=media&token=e70fbb32-e7b8-40fe-83e8-a199404b1125"
+                    userHashMap["cover"] =
+                        "https://firebasestorage.googleapis.com/v0/b/chattingapp-22b4a.appspot.com/o/cover.jpeg?alt=media&token=e98859f9-90cd-4932-9aea-7b18b39f10f4"
+                    userHashMap["status"] = "offline"
+                    userHashMap["search"] = username.toLowerCase()
+                    userHashMap["facebook"] = "https://m.facebook.com"
+                    userHashMap["instagram"] = "https://m.instagram.com"
+                    userHashMap["website"] = "https://www.google.com"
+
+                    refUsers.updateChildren(userHashMap).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+
+                } else {
+                    Toast.makeText(
+                        this, getString(R.string.text_error_message) + task.exception!!
+                            .message.toString(), Toast.LENGTH_LONG
+                    ).show()
+
+                }
+            }
+        }
     }
 }
